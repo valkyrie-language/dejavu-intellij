ff8eeaec0bd4162a133167e058c54e928ea10f06
Aster <aster@vers.site>
2023-01-27 10:04:02 +0000

    🧨 Remove library and sdk support

--- src/main/kotlin/valkyrie/ide/project/library/ValkyrieLibrarySettings.kt
+++ src/main/kotlin/valkyrie/ide/project/library/ValkyrieLibrarySettings.kt
@@ -1,18 +1,8 @@
 package valkyrie.ide.project.library
 
 import com.intellij.openapi.options.Configurable
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.libraries.LibraryKind
-import com.intellij.openapi.roots.ui.configuration.LibrarySettingsProvider
 import com.intellij.ui.dsl.builder.panel
 import javax.swing.JComponent
-
-class ValkyrieLibrarySettings : LibrarySettingsProvider() {
-    override fun getLibraryKind(): LibraryKind = ValkyrieLibraryType.kind
-    override fun getAdditionalSettingsConfigurable(project: Project?): Configurable {
-        return LibrarySettingConfigurable()
-    }
-}
 
 private class LibrarySettingConfigurable : Configurable {
     override fun createComponent(): JComponent? {
