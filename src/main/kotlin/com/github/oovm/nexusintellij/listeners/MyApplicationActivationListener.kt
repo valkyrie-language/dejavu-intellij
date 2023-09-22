275dc95c4b257d83fae33688bbf8043a0f4256dc
GitHub Action <action@github.com>
2023-09-22 20:23:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/oovm/nexusintellij/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/oovm/nexusintellij/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.oovm.nexusintellij.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
