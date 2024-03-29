f3121c4c75a43f9beb2f8b22b5b64bcea50bb7da
Aster <aster@vers.site>
2024-03-30 00:46:22 +0800

    Optimize query algorithm for folding position

--- src/main/kotlin/valkyrie/ide/folding/FoldingOptions.kt
+++ src/main/kotlin/valkyrie/ide/folding/FoldingOptions.kt
@@ -4,7 +4,7 @@ import com.intellij.application.options.editor.CodeFoldingOptionsProvider
 import javax.swing.JComponent
 
 /// 显示在 Editor > General > Code Folding 中
-class ValkyrieFoldingOptions : CodeFoldingOptionsProvider {
+class FoldingOptions : CodeFoldingOptionsProvider {
     override fun createComponent(): JComponent? {
         return null
     }
